2551a4de28939cb7a4ea7def4b0bc336f91cb88d
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-09-12 15:50:58 +0300

    added hotel detail page and e2e test

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -153,3 +153,12 @@ export const searchHotels = async (searchParams: SearchParams):Promise<HotelSear
     }
     return response.json();
 }
+
+export const fetchHotelById = async (hotelId: string): Promise<HotelType> => {
+    const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`);
+    if (!response.ok) {
+      throw new Error("Error fetching Hotels");
+    }
+  
+    return response.json();
+  };
